bc1f477eece74f6e5468014dfa73bf4eeef0d309
makmeghana <51366035+makmeghana@users.noreply.github.com>
2019-06-17 11:40:37 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,18 @@
+/*
+ * ledlcd.c
+ *
+ * Created: 07-06-2019 14:27:21
+ * Author : ganga
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
